34de4dc0c19909b5b29506810073ffb9780f1d1c
HannaRassokha <32023002+HannaRassokha@users.noreply.github.com>
2017-09-18 16:50:35 +0300

    added ExtentionForString.sln

---
+++ ExtentionForString/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExtentionForString;
+
+namespace ExtentionForString
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please, enter you string: \n");
+            var original = Console.ReadLine().ReverseWords();
+            Console.WriteLine("Reversed: {0}", original);
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ ExtentionForString/ReverseWords.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtentionForString
+{
+    public static class ExtentionMethod
+    {
+        public static string ReverseWords(this string original)
+        {
+            char[] chars = original.ToCharArray();
+            ReverseCharArray(chars, 0, chars.Length - 1);
+            int wordStart = 0;
+            while (wordStart < chars.Length)
+            {
+                while (wordStart < chars.Length - 1 &&
+                       !char.IsLetter(chars[wordStart]))
+                    wordStart++;
+
+                int wordEnd = wordStart;
+                while (wordEnd < chars.Length - 1 &&
+                       char.IsLetter(chars[wordEnd + 1]))
+                    wordEnd++;
+
+                if (wordEnd > wordStart)
+                {
+                    ReverseCharArray(chars, wordStart, wordEnd);
+                }
+
+                wordStart = wordEnd + 1;
+            }
+            return new string(chars);
+        }
+
+        static void ReverseCharArray(char[] chars, int left, int rigth)
+        {
+            int l = left;
+            int r = rigth;
+            while (l < r)
+            {
+                char temp = chars[l];
+                chars[l] = chars[r];
+                chars[r] = temp;
+
+                l++;
+                r--;
+
+            }
+
+        }
+    }
+}
